7a24acd9114c636d65be25b5eeb0f605d14f6c47
Abdul Jamal Wahid <91882266+Abdul-Jamal-Wahid@users.noreply.github.com>
2024-06-14 00:20:42 +0530

    Time: 15 ms (15.29%), Space: 7 MB (71.95%) - LeetHub

---
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int fib(int n) {
+        if(n==0){
+            return 0;
+        }else if(n==1){
+            return 1;
+        }
+        int x=fib(n-1)+fib(n-2);
+        return x;
+    }
+};
